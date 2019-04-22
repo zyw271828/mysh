36c33035033e2468bb8a890e184ff6ca6a790e96
zyw271828 <zyw_0618@outlook.com>
2019-04-22 20:15:03 +0800

    Integrated modules to mysh

--- mysh.c
+++ mysh.c
@@ -6,20 +6,26 @@
 #include <sys/wait.h>
 
 #define PROGRAM_NAME "mysh"
+#define MYSH "/path/to/mysh/"
+#define MAX_ARGV_NUM 100
+#define MAX_ARGV_LEN 1000
+#define MAX_PATH_LEN 1024
 
 int call_cmd(char *cmd_argv[]);
 
 int main(int argc, char *argv[])
 {
-    char input[1000];
+    char input[(MAX_ARGV_NUM) * (MAX_ARGV_LEN)];
     char *cmds;
-    char *cmd[100][1000];
+    char cmd[MAX_ARGV_NUM][MAX_ARGV_LEN];
+    char *cmdp[MAX_ARGV_NUM][MAX_ARGV_LEN];
 
     chdir(getenv("HOME"));
 
     while (1)
     {
-        char cwd[1024];
+        // Print shell prompt
+        char cwd[MAX_PATH_LEN];
         getcwd(cwd, sizeof(cwd));
         if (strcmp(cwd, getenv("HOME")) == 0)
         {
@@ -34,35 +40,85 @@ int main(int argc, char *argv[])
 
         // TODO: Auto-completion through tab by using getchar()
         // FIXME: Infinite loop when Ctrl-D is pressed
-        fgets(input, 1000, stdin);
+        // Convert input to argv array and store it in cmd
+        // cmdp are pointers to each argv
+        fgets(input, (MAX_ARGV_NUM) * (MAX_ARGV_LEN), stdin);
         cmds = strtok(input, " \n");
+        int cmd_argc = 0;
 
-        for (int i = 0; cmds != NULL; i++)
+        for (cmd_argc = 0; cmds != NULL; cmd_argc++)
         {
-            (*cmd)[i] = cmds;
-            (*cmd)[i + 1] = NULL;
+            strcpy(cmd[cmd_argc], cmds);
+            (*cmdp)[cmd_argc] = cmd[cmd_argc];
+            (*cmdp)[cmd_argc + 1] = NULL; // Must be terminated by a null pointer
             cmds = strtok(NULL, " \n");
         }
 
-        if (strcmp((*cmd)[0], "cd") == 0)
+        // Find the command to be executed
+        if (strcmp(cmd[0], "cd") == 0)
         {
-            if ((*cmd)[1] == NULL) // Execute in `cd`
+            if (cmd[1] == NULL || cmd[1][0] == '\0' || strcmp(cmd[1], "~") == 0) // Execute in `cd` or `cd ~`
             {
                 chdir(getenv("HOME"));
             }
             else // Execute in `cd DIR`
             {
-                if (chdir((*cmd)[1]) != 0)
+                if (chdir(cmd[1]) != 0)
                 {
                     printf("cd");
                     printf(": cannot change directory to \'");
-                    printf("%s\'\n", (*cmd)[1]);
+                    printf("%s\'\n", cmd[1]);
                 }
             }
         }
-        else if (strcmp((*cmd)[0], "pwd") == 0)
+        else if (strcmp(cmd[0], "pwd") == 0)
         {
             printf("%s\n", cwd);
+        }
+        else if (strcmp(cmd[0], "ls") == 0)
+        {
+            (*cmdp)[0] = MYSH "ls";
+            call_cmd(cmdp[0]);
+        }
+        else if (strcmp(cmd[0], "echo") == 0)
+        {
+            (*cmdp)[0] = MYSH "echo";
+            call_cmd(cmdp[0]);
+        }
+        else if (strcmp(cmd[0], "cat") == 0)
+        {
+            (*cmdp)[0] = MYSH "cat";
+            call_cmd(cmdp[0]);
+        }
+        else if (strcmp(cmd[0], "mkdir") == 0)
+        {
+            (*cmdp)[0] = MYSH "mkdir";
+            call_cmd(cmdp[0]);
+        }
+        else if (strcmp(cmd[0], "rm") == 0)
+        {
+            (*cmdp)[0] = MYSH "rm";
+            call_cmd(cmdp[0]);
+        }
+        else if (strcmp(cmd[0], "cd") == 0)
+        {
+            (*cmdp)[0] = MYSH "cd";
+            call_cmd(cmdp[0]);
+        }
+        else if (strcmp(cmd[0], "wc") == 0)
+        {
+            (*cmdp)[0] = MYSH "wc";
+            call_cmd(cmdp[0]);
+        }
+        else
+        {
+            printf("%s: command not found\n", cmd[0]);
+        }
+
+        // Clean up the argv array
+        for (int i = 0; i < cmd_argc; i++)
+        {
+            cmd[i][0] = '\0';
         }
     }
     return 0;
