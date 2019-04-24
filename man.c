c125a5353c709fc67a92ed9cddf974585ee02aa0
zyw271828 <zyw_0618@outlook.com>
2019-04-24 22:39:31 +0800

    Added manual for cd, pwd and ls

--- man.c
+++ man.c
@@ -21,12 +21,35 @@ int main(int argc, char *argv[])
             // Find the command manual that needs to be printed
             if (strcmp(argv[i], "cd") == 0)
             {
+                printf("NAME\n");
+                printf("       cd - change the working directory\n\n");
+                printf("SYNOPSIS\n");
+                printf("       cd [directory]\n\n");
+                printf("DESCRIPTION\n");
+                printf("       The cd command can change the current working directory.\n\n");
+                printf("       If no directory operand is given and the HOME environment variable is set to a\n");
+                printf("       non-empty value, the cd utility shall behave as if the directory named in the\n");
+                printf("       HOME environment variable was specified as the directory operand.\n");
             }
             else if (strcmp(argv[i], "pwd") == 0)
             {
+                printf("NAME\n");
+                printf("       pwd - print name of current/working directory\n\n");
+                printf("SYNOPSIS\n");
+                printf("       pwd\n\n");
+                printf("DESCRIPTION\n");
+                printf("       Print the full filename of the current working directory.\n");
             }
             else if (strcmp(argv[i], "ls") == 0)
             {
+                printf("NAME\n");
+                printf("       ls - list directory contents\n\n");
+                printf("SYNOPSIS\n");
+                printf("       ls [directory]\n\n");
+                printf("DESCRIPTION\n");
+                printf("       List information about the FILEs (the current directory by default).\n\n");
+                printf("       The meaning of each field is: file permissions, number of links, owner name,\n");
+                printf("       owner group, file size, time of last modification, file name.\n");
             }
             else if (strcmp(argv[i], "echo") == 0)
             {
