a6237163dd8283ab6f129c2f2090ed6c86c02959
zyw271828 <zyw_0618@outlook.com>
2019-04-25 22:11:55 +0800

    Implemented exit

--- mysh.c
+++ mysh.c
@@ -113,6 +113,11 @@ int main(int argc, char *argv[])
             (*cmdp)[0] = MYSH "man";
             call_cmd(cmdp[0]);
         }
+        else if (strcmp(cmd[0], "exit") == 0)
+        {
+            printf("exit\n");
+            exit(0);
+        }
         else
         {
             printf("%s: command not found\n", cmd[0]);
